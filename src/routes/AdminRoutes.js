1ce5c67a486b200b6b567c683be12d64e210fce7
ValaBindiya <bindiyavala6@gmail.com>
2024-03-19 11:07:10 +0530

    19/3/2024_Product_Table

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Route, Router, Routes } from 'react-router-dom';
 import Product from '../admine/container/Product/Product';
+import Layout from '../admine/component/Layout/Layout';
+import Reviews from '../admine/container/Reviews/Reviews';
 
 function AdminRoutes(props) {
     return (
 
+        <Layout >
             <Routes >
                 <Route exact path='/Product' element={<Product />} />
+                <Route exact path='/Reviews' element={<Reviews />} />
             </Routes>
+        </Layout>
+
     );
 }
 
